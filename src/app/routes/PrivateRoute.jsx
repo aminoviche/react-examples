6145dae830c2b6bc6fd8f8a267b1c26b6428ec5e
Amine Fouzi <amine.faouzi@external.idemia.com>
2025-10-28 09:04:58 +0100

    disable keycloack

--- src/app/routes/PrivateRoute.jsx
+++ src/app/routes/PrivateRoute.jsx
@@ -5,21 +5,21 @@ import { useKeycloak } from '@react-keycloak/web';
 
 
 const PrivateRoute = ({ children }) => {
-    const { keycloak, initialized } = useKeycloak();
-
-    useEffect(() => {
-        if (initialized && !keycloak.authenticated) {
-            keycloak.login();
-        }
-    }, [initialized, keycloak]);
-
-    if (!initialized) {
-        return <div>Loading...</div>; // Better loading indicator
-    }
-
-    if (!keycloak.authenticated) {
-        return <div>Authenticating...</div>; // Better indicator
-    }
+    // const { keycloak, initialized } = useKeycloak();
+    //
+    // useEffect(() => {
+    //     if (initialized && !keycloak.authenticated) {
+    //         keycloak.login();
+    //     }
+    // }, [initialized, keycloak]);
+    //
+    // if (!initialized) {
+    //     return <div>Loading...</div>; // Better loading indicator
+    // }
+    //
+    // if (!keycloak.authenticated) {
+    //     return <div>Authenticating...</div>; // Better indicator
+    // }
 
     return children;
 };
